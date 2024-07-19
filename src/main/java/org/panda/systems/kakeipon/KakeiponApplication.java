57a2b03fa08cd67a2bfb6535935cade096de162a
Yoshihiro NAKAHARA <yoshihiro.n@panda-systems.org>
2024-07-19 11:53:22 +0900

    Initial commit

---
+++ src/main/java/org/panda/systems/kakeipon/KakeiponApplication.java
@@ -0,0 +1,13 @@
+package org.panda.systems.kakeipon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakeiponApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KakeiponApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/panda/systems/kakeipon/KakeiponApplicationTests.java
@@ -0,0 +1,13 @@
+package org.panda.systems.kakeipon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KakeiponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
