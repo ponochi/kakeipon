2e155c3e2735db7b1bffd81fb33c680236f4b706
Yoshihiro NAKAHARA <yoshihiro.n@panda-systems.org>
2024-08-16 11:17:00 +0900

    Implements to entry of source and destination account.

--- src/main/java/org/panda/systems/kakeipon/app/common/AccountController.java
+++ src/main/java/org/panda/systems/kakeipon/app/common/AccountController.java
@@ -8,7 +8,10 @@ import org.panda.systems.kakeipon.domain.service.spec.SpecificationGroupService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.util.UriComponentsBuilder;
 
 @Controller
